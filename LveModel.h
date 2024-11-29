9ac5fd5da8692d2ce9dd0d7db41cbe66fe8e5ae3
Juliano Pasa <julianodemellopasa@gmail.com>
2024-11-29 10:55:09 -0300

    Episode 16

--- LveModel.h
+++ LveModel.h
@@ -23,7 +23,13 @@ namespace lve
 			static std::vector<VkVertexInputAttributeDescription> GetAttributeDescriptions();
 		};
 
-		LveModel(LveDevice& device, const std::vector<Vertex>& vertices);
+		struct Builder
+		{
+			std::vector<Vertex> vertices{};
+			std::vector<uint32_t> indices{};
+		};
+
+		LveModel(LveDevice& device, const LveModel::Builder &builder);
 		~LveModel();
 
 		LveModel(const LveModel&) = delete;
@@ -37,7 +43,13 @@ namespace lve
 		VkBuffer vertexBuffer;
 		VkDeviceMemory vertexBufferMemory;
 		uint32_t vertexCount;
+		
+		bool hasIndexBuffer = false;
+		VkBuffer indexBuffer;
+		VkDeviceMemory indexBufferMemory;
+		uint32_t indexCount;
 
 		void CreateVertexBuffers(const std::vector<Vertex>& vertices);
+		void CreateIndexBuffers(const std::vector<uint32_t>& indices);
 	};
 }
